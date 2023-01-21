ea0d1fd3fe940f71664930e011590b51db0ec277
Giselle <gisalina0618@gmail.com>
2023-01-21 19:53:59 -0400

    OperadoresLog

---
+++ Oper.js
@@ -0,0 +1,149 @@
+let mestexto = "10";
+let diahoy = 17;
+let diaayer = 16;
+let meshoy = 10;
+let mesayer = 10;
+let comparacion;
+
+
+if(diahoy > diaayer)
+{
+comparacion = "VERDADERO"
+}
+else
+{
+comparacion = "FALSO"
+}
+
+document.write("diaHoy ",diahoy, " es mayor que diaAyer ", diaayer,": ", comparacion)
+
+document.write("<br>")
+
+if(diahoy < diaayer)
+{
+comparacion = "VERDADERO"
+}
+else
+{
+comparacion = "FALSO"
+}
+
+document.write("diaHoy ", diahoy, " es menor que diaAyer ", diaayer, ": ", comparacion)
+
+document.write("<br>")
+
+if(diahoy >= diaayer)
+{
+comparacion = "VERDADERO"
+}
+else
+{
+comparacion = "FALSO"
+}
+
+document.write("diaHoy ", diahoy, " es mayor o igual que diaAyer ", diaayer, ": ", comparacion)
+
+document.write("<br>")
+
+if(meshoy >= mesayer)
+{
+comparacion = "VERDADERO"
+}
+else
+{
+comparacion = "FALSO"
+}
+
+document.write("mesHoy ", meshoy," es mayor o igual que mesAyer ", mesayer, ": ", comparacion)
+
+document.write("<br>")
+
+if(diahoy <= diaayer)
+{
+comparacion = "VERDADERO"
+}
+else
+{
+comparacion = "FALSO"
+}
+
+document.write("diaHoy ", diahoy, " es menor o igual que diaAyer ", diaayer,": ", comparacion)
+
+document.write("<br>")
+
+if(meshoy <= mesayer)
+{
+comparacion = "VERDADERO"
+}
+else
+{
+comparacion = "FALSO"
+}
+
+document.write("mesHoy ", meshoy, " es menor o igual que mesAyer ", mesayer, ": ", comparacion)
+
+document.write("<br>")
+
+if(mestexto == mesayer)
+{
+comparacion = "VERDADERO"
+}
+else
+{
+comparacion = "FALSO"
+}
+
+document.write("mesTexto ", mestexto, " es igual que mesAyer ", mesayer, ": ", comparacion)
+
+document.write("<br>")
+
+if(diahoy == diaayer)
+{
+comparacion = "VERDADERO"
+}
+else
+{
+comparacion = "FALSO"
+}
+
+document.write("diaHoy ", diahoy, " es igual que diaAyer ", diaayer, ": ", comparacion)
+
+document.write("<br>")
+
+if(meshoy == mesayer)
+{
+comparacion = "VERDADERO"
+}
+else
+{
+comparacion = "FALSO"
+}
+
+document.write("mesHoy ", meshoy, " es igual que mesAyer ", mesayer, ": ", comparacion)
+
+document.write("<br>")
+
+if(meshoy === mesayer)
+{
+comparacion = "VERDADERO"
+}
+else
+{
+comparacion = "FALSO"
+}
+
+document.write("mesHoy ", meshoy, " es idéntico que mesAyer ", mesayer, ": ", comparacion)
+
+document.write("<br>")
+
+if(mestexto === mesayer)
+{
+comparacion = "VERDADERO"
+}
+else
+{
+comparacion = "FALSO"
+}
+
+document.write("mesTexto ", mestexto, " es idéntico que mesAyer ", mesayer, ": ", comparacion)
+
